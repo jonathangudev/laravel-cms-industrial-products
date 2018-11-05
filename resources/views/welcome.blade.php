87b3a7e988e143eed3230d9d7e4027db1ee52ef9
Andrew Barnhart <abarnhart@vividfront.com>
2018-11-05 16:58:29 -0500

    Added color styles. Added typography. Added buttons and breadcrumbs. Added utilities and other tools.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,58 +1,158 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>JMP</title>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,400i,600,700" rel="stylesheet" type="text/css">
+	<title>JMP</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('/css/index.css') }}">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,400i,600,700" rel="stylesheet" type="text/css">
 
-        <!-- Favicons -->
-        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/images/favicons/apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/images/favicons/apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/images/favicons/apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/favicons/apple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/images/favicons/apple-touch-icon-60x60.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/images/favicons/apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/images/favicons/apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/favicons/apple-touch-icon-152x152.png" />
-        <link rel="icon" type="image/png" href="/images/favicons/favicon-196x196.png" sizes="196x196" />
-        <link rel="icon" type="image/png" href="/images/favicons/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/png" href="/images/favicons/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/images/favicons/favicon-16x16.png" sizes="16x16" />
-        <link rel="icon" type="image/png" href="/images/favicons/favicon-128.png" sizes="128x128" />
-        <meta name="theme-color" content="#ffffff">
-        <meta name="application-name" content="JMP Industries Inc."/>
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/images/favicons/mstile-144x144.png" />
-        <meta name="msapplication-square70x70logo" content="/images/favicons/mstile-70x70.png" />
-        <meta name="msapplication-square150x150logo" content="/images/favicons/mstile-150x150.png" />
-        <meta name="msapplication-wide310x150logo" content="/images/favicons/mstile-310x150.png" />
-        <meta name="msapplication-square310x310logo" content="/images/favicons/mstile-310x310.png" />
-    </head>
+	<!-- Styles -->
+	<link rel="stylesheet" href="{{ mix('/css/index.css') }}">
 
-    <body>
+	<!-- Favicons -->
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/images/favicons/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/images/favicons/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/images/favicons/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/favicons/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="/images/favicons/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/images/favicons/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="/images/favicons/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/favicons/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="/images/favicons/favicon-196x196.png" sizes="196x196" />
+	<link rel="icon" type="image/png" href="/images/favicons/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="/images/favicons/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="/images/favicons/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="/images/favicons/favicon-128.png" sizes="128x128" />
+	<meta name="theme-color" content="#ffffff">
+	<meta name="application-name" content="JMP Industries Inc." />
+	<meta name="msapplication-TileColor" content="#ffffff" />
+	<meta name="msapplication-TileImage" content="/images/favicons/mstile-144x144.png" />
+	<meta name="msapplication-square70x70logo" content="/images/favicons/mstile-70x70.png" />
+	<meta name="msapplication-square150x150logo" content="/images/favicons/mstile-150x150.png" />
+	<meta name="msapplication-wide310x150logo" content="/images/favicons/mstile-310x150.png" />
+	<meta name="msapplication-square310x310logo" content="/images/favicons/mstile-310x310.png" />
+</head>
+
+<body>
+    <header></header>
+
+    <main>
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h1>JMP</h1>
+                    <nav class="c-breadcrumbs">
+                        <ul class="c-breadcrumbs__list">
+                            <li class="c-breadcrumbs__item"><a href="/">Home</a></li>
+                            <li class="c-breadcrumbs__item"><a href="/">Previous Page</a></li>
+                            <li class="c-breadcrumbs__item"><strong>Current Page</strong></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+    
+            <div class="row">
+                <div class="col">
+                    <h2 class="c-section-title">Section Title</h2>
+                    <h1>Heading 1</h1>
+                    <h2>Heading 2</h2>
+                    <h3>Heading 3</h3>
+                    <h4>Heading 4</h4>
+                    <h5>Heading 5</h5>
+                    <h6>Heading 6</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut euismod tortor, vel fermentum ante.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut eget lectus vitae
+                        libero commodo bibendum molestie nec felis. Aenean scelerisque nulla felis, a lobortis ligula tempor sit amet.
+                        Integer in imperdiet justo. Maecenas tempor sagittis elementum. Fusce sit amet eros lectus. Sed mollis sodales
+                        orci, vel porta sapien iaculis quis. Nullam tincidunt augue nec justo efficitur blandit. Aliquam vulputate ex ac
+                        justo rhoncus hendrerit. Morbi pharetra fringilla euismod.</p>
+    
+                    <p><a href="#">Link</a></p>
+    
+                    <p>
+                        <a href="#" class="c-btn c-btn--primary">Button</a>
+                        <a href="#" class="c-btn c-btn--primary c-btn--ghost">Button</a>
+                    </p>
+    
+                    <form action="/" class="o-form">
+                        <div class="o-form__group">
+                            <div class="o-form__search">
+                                <label for="vf-search">Search Products</label>
+                                <input type="search" id="vf-search" placeholder="Search products...">
+                                <button type="submit" class="c-btn" ><i class="fas fa-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+    
+                    <form action="/" class="o-form">
+                        <div class="o-form__group">
+                            <label for="vf-text">Label</label>
+                            <input type="text" id="vf-text" placeholder="e.g. Placeholder text">
+                        </div>
+    
+                        <div class="o-form__group">
+                            <label for="vf-text">Label</label>
+                            <input type="text" id="vf-text" placeholder="e.g. Placeholder text" value="This has text entered.">
+                        </div>
+    
+                        <div class="o-form__group is-disabled">
+                            <label for="vf-text">Label - Disabled</label>
+                            <input type="text" id="vf-text" placeholder="e.g. Placeholder text" disabled>
+                        </div>
+    
+                        <div class="o-form__group is-error">
+                            <label for="vf-text">Label - Error</label>
+                            <input class="is-error" type="text" id="vf-text" placeholder="e.g. Placeholder text" value="This text has errors.">
+                            <p class="o-form__error-text"><i class="fas fa-times"></i>&nbsp;&nbsp;<span>This is some error text.</strong></p>
+                        </div>
+    
+                        <div class="o-form__group">
+                            <label for="vf-textare">Label</label>
+                            <textarea name="textarea" id="vf-textarea" rows="5" placeholder="e.g. Placeholder text"></textarea>
+                        </div>
+    
+                        <div class="o-form__group">
+                            <input type="checkbox" name="checkbox" id="vf-check">
+                            <label for="vf-check"><strong>Checkbox</strong> - Label with text<br />that goes onto second line</label>
+                        </div>
+        
+                        <div class="o-form__group is-error">
+                            <input class="is-error" type="checkbox" name="checkbox" id="vf-check">
+                            <label for="vf-check"><strong>Checkbox</strong> - Label with text<br />that goes onto second line</label>
+                            <p class="o-form__error-text"><i class="fas fa-times"></i>&nbsp;&nbsp;<span>This is some error text.</strong></p>
+                        </div>
+    
+                        <div class="o-form__group">
+                            <input type="radio" name="radio" id="vf-radio">
+                            <label for="vf-radio"><strong>Radio</strong> - Label with text that<br />goes onto second line</label>
+                        </div>
+                        
+                        <div class="o-form__group is-error">
+                            <input class="is-error" type="radio" name="radio" id="vf-radio">
+                            <label for="vf-radio"><strong>Radio</strong> - Label with text that<br />goes onto second line</label>
+                            <p class="o-form__error-text"><i class="fas fa-times"></i>&nbsp;&nbsp;<span>This is some error text.</strong></p>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </main>
 
-        @if (Route::has('login'))
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
-                <a href="{{ route('register') }}">Register</a>
-            @endauth
-        @endif
-    </body>
+    <footer></footer>
+    
+
+	@if (Route::has('login'))
+        @auth
+            <a href="{{ url('/home') }}">Home</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+            <a href="{{ route('register') }}">Register</a>
+        @endauth
+	@endif
+</body>
+
 </html>
