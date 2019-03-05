549e579ee0551a85cf92845949bf91cc89ae7d78
John Gu <johnguiscool@gmail.com>
2019-03-05 16:25:47 -0500

    first commit

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Catalog\Category;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
 class CatalogController extends Controller
 {
     /**
@@ -21,6 +25,18 @@ class CatalogController extends Controller
      */
     public function index()
     {
-        return view('categories');
+
+        $userCompany = Auth::user()->company_id; 
+
+        if(empty($userCompany))
+        {
+            abort(403);
+        }
+        
+        $categories = Category::whereisRoot()
+            ->where('company_id', 1)
+            ->get();
+
+        return view('categories', ['categories' => $categories]);
     }
 }
