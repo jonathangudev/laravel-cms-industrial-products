90a2071fdc7c82af0932b435453a8c0a4ac87f5a
Nevin Morgan <nevin@vividfront.com>
2019-03-12 16:35:53 -0400

    removed verified middleware

--- app/Http/Controllers/RestrictedAssetController.php
+++ app/Http/Controllers/RestrictedAssetController.php
@@ -16,7 +16,7 @@ class RestrictedAssetController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['auth', 'verified']);
+        $this->middleware(['auth']);
     }
 
     /**
