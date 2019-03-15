45c93aa7fb42ac6755e1ff16f4c5c73fdccd182a
John Gu <johnguiscool@gmail.com>
2019-03-15 14:58:28 -0400

    default search engine

--- config/scout.php
+++ config/scout.php
@@ -11,11 +11,11 @@ return [
     | using Laravel Scout. This connection is used when syncing all models
     | to the search service. You should adjust this based on your needs.
     |
-    | Supported: "algolia", "null"
+    | Supported: "tntsearch", algolia", "null"
     |
     */
 
-    'driver' => env('SCOUT_DRIVER', 'algolia'),
+    'driver' => env('SCOUT_DRIVER', 'tntsearch'),
 
     /*
     |--------------------------------------------------------------------------
