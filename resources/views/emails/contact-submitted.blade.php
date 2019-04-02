060f2c4a65e5c1c7238db20f60417a53f669227e
John Gu <johnguiscool@gmail.com>
2019-04-02 16:06:41 -0400

    changed to markdown and shouldqueue

--- resources/views/emails/contact-submitted.blade.php
+++ resources/views/emails/contact-submitted.blade.php
@@ -1,5 +1,9 @@
+@component('mail::message')
+#Contact Submitted
+
 Name: {{$contactName}} <br>
 Company: {{$contactCompany}} <br>
-Email:  {{$contactEmail}} <br>
-Phone:  {{$contactPhone}} <br>
-Message:  {{$contactMessage}}
+Email: {{$contactEmail}} <br>
+Phone: {{$contactPhone}} <br>
+Message: {{$contactMessage}}
+@endcomponent 
