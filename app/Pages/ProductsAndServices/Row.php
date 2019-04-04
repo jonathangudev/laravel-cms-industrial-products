42e6e5bfa5d7bb7cdbdeb786f84690239404fc97
John Gu <johnguiscool@gmail.com>
2019-04-04 12:02:15 -0400

    initial

---
+++ app/Pages/ProductsAndServices/Row.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Pages\ProductsAndServices;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Row extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'pages_products_and_services_rows';
+}
